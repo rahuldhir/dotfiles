44bbeaf1aceeed0e37b9c13b3b5c016aab7fd5ce
Rahul Dhir <me@rahuldhir.com>
2021-06-14 18:45:09 -0500

    Customize startify screen

--- .config/nvim/lua/plugins/config/startify.lua
+++ .config/nvim/lua/plugins/config/startify.lua
@@ -4,11 +4,14 @@ vim.g.startify_change_to_vcs_root = true
 local version = string.match(vim.api.nvim_exec('version', true), 'NVIM (.*)\nBuild')
 vim.g.startify_custom_header = "startify#pad(['Neovim " .. version .. "'])"
 
--- vim.g.startify_lists = [
---   { 'type': 'dir',       'header': ['   MRU '. getcwd()] },
---   { 'type': 'sessions',  'header': ['   Sessions']       },
---   { 'type': 'bookmarks', 'header': ['   Bookmarks']      },
---   { 'type': 'commands',  'header': ['   Commands']       },
--- ]
+vim.g.startify_lists = {
+    { type = 'dir',       header = {'   MRU '..vim.fn.getcwd()} },
+    { type = 'files',     header = {'   MRU'} },
+    { type = 'bookmarks', header = {'   Bookmarks'}      },
+    { type = 'sessions',  header = {'   Sessions'}       },
+    { type = 'commands',  header = {'   Commands'}       },
+}
 
--- vim.g.startify_bookmarks = [ {'d': '~/.config'} ]
+vim.g.startify_bookmarks = {
+    '~/.config/',
+}
