04b7f98cce32e7971cf507bd7979aef40688003b
Rahul Dhir <rahul.dhir@wpengine.com>
2021-07-06 11:41:11 -0500

    Update nvim-tree config

--- .config/nvim/lua/plugins/config/nvim-tree.lua
+++ .config/nvim/lua/plugins/config/nvim-tree.lua
@@ -1,6 +1,4 @@
-local function get_lua_cb(cb_name)
-  return string.format(":lua require'nvim-tree'.on_keypress('%s')<CR>", cb_name)
-end
+local tree_cb = require'nvim-tree.config'.nvim_tree_callback
 
 -- https://github.com/kyazdani42/nvim-tree.lua/blob/905afba20900caea1e6f0f541c2ed7302de9f598/lua/nvim-tree/config.lua#L61-L85
 vim.g.nvim_tree_width = 40
@@ -10,8 +8,8 @@ vim.g.nvim_tree_auto_close = 1
 vim.g.nvim_tree_quit_on_open = 1
 vim.g.nvim_tree_follow = 1
 vim.g.nvim_tree_bindings = {
-  ["<Tab>"] = "<C-W>W",
-  ["."] = "H",
-  ["l"] = ":lua require'nvim-tree'.on_keypress('edit')<CR>",
-  ["h"] = ":lua require'nvim-tree'.on_keypress('close_node')<CR>",
+  { key = "<Tab>", cb = "<C-W>W", },
+  { key = ".",     cb = tree_cb("toggle_dotfiles"), },
+  { key = "l",     cb = tree_cb("edit"), },
+  { key = "h",     cb = tree_cb("close_node"), },
 }
