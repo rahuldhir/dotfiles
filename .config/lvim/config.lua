b9f1255977a55f132657b7b3046e7d2f4c499caa
Rahul Dhir <rahul.dhir@wpengine.com>
2023-05-24 15:05:27 -0500

    Update lvim config

--- .config/lvim/config.lua
+++ .config/lvim/config.lua
@@ -1,8 +1,13 @@
 lvim.colorscheme = "gruvbox-material"
-lvim.plugins = {{"sainnhe/gruvbox-material"}}
+lvim.plugins = { { "sainnhe/gruvbox-material" } }
+lvim.format_on_save = true
 
 require("lvim.lsp.null-ls.formatters").setup({
-	{ exe = "goimports", filetypes = { "go" } },
+  { exe = "goimports", filetypes = { "go" } },
 })
 
-vim.o.cmdheight=0
+require("lvim.lsp.null-ls.linters").setup({
+  { command = "golangci-lint", filetypes = { "go" } },
+})
+
+vim.o.cmdheight = 0
